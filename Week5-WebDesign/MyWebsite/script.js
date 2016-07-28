9d9ec9e4698ff3bfc6a30987addcc643cd573271
Alexsouthwick <acsouthwick@gmail.com>
2016-07-28 16:01:45 -0700

    Updated my portfolio

--- Week5-WebDesign/MyWebsite/script.js
+++ Week5-WebDesign/MyWebsite/script.js
@@ -1,25 +1,38 @@
 $(document).ready(function() {
-    $("#pinterest_tour").hide();
-    $("#pinterest_lunch").hide();
-    $('#next').on("click", change_picture);
+    // $("#pinterest_tour").hide();
+    // $("#pinterest_lunch").hide();
+    $("button").on("click", plusDivs(+1));
+    $("button").on("click", plusDivs(-1));
 });
 
 
-function change_picture(){
-    var original_picture= '#pinterest_tour'
-    var new_picture = '#pinterest_sign'
-        var new_src= $(original_picture).attr('src');
-        $(new_picture).attr('src', new_src);
-     original_picture='#pinterest_sign'
-     new_picture= '#pinterest_lunch'
 
-        $(new_picture).attr('src', new_src);
+        var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("photos");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
     }
+    x[slideIndex-1].style.display = "block"; 
+}
 
 
+// var original_picture= '#pinterest_tour'
+//     var new_picture = '#pinterest_sign'
+//         var new_src= $(original_picture).attr('src');
+//         $(new_picture).attr('src', new_src);
+//      original_picture='#pinterest_sign'
+//      new_picture= '#pinterest_lunch'
 
-    // var source= $('#pinterest_sign.src');
-    // console.log(source);
-    // source= $('#pinterest_tour.src');
-    // console.log(source);
+//         $(new_picture).attr('src', new_src);
+
 
